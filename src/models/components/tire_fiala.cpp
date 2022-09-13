9cef87d4466daf9b9c23ef8920c559597145d72c
Wei ZHANG <wei_zhang_1988@outlook.com>
2022-09-13 15:10:47 +0800

    add frame transfer in tire

--- src/models/components/tire_fiala.cpp
+++ src/models/components/tire_fiala.cpp
@@ -90,10 +90,32 @@ void NMSPC::Tire_Fiala::update_pv (const d_vec &inputs, d_vec &outputs) {
 	m_Whl_omega = inputs[0];
 	m_Sus_vx = inputs[1];
 	m_Sus_vy = inputs[2];
-	m_Sus_gamma = inputs[3];
-	m_Sus_psidot = inputs[4];
-	m_Whl_Re = inputs[5];
-	m_Whl_rhoz = inputs[6];
+	m_Whl_vz = inputs[3];
+	m_Sus_gamma = inputs[4];
+	m_Sus_str = inputs[5];
+	m_Sus_psidot = inputs[6];
+	m_Whl_Re = inputs[7];
+	m_Whl_rhoz = inputs[8];
+	//frame transfer
+	m_Tir_psidot = -m_Sus_psidot;
+	m_Tir_gamma = m_Sus_gamma + pi;
+	m_Tir_str = m_Sus_str;
+	m_cos_a = cos(m_Tir_gamma);
+	m_cos_c	= cos(m_Tir_str);
+	m_sin_a = sin(m_Tir_gamma);
+	m_sin_c	= sin(m_Tir_str);
+	m_DCM_00 = m_cos_a;
+	m_DCM_01 = m_sin_a;
+	m_DCM_02 = 0.0;
+	m_DCM_10 = -m_cos_c * m_sin_a;
+	m_DCM_11 = m_cos_c * m_cos_a;
+	m_DCM_12 = m_sin_c;
+	m_DCM_20 = m_sin_c * m_sin_a;
+	m_DCM_21 = -m_sin_c * m_cos_a;
+	m_DCM_22 = m_cos_c;
+	m_Tir_vx = m_DCM_00 * m_Sus_vx + m_DCM_10 * m_Sus_vy + m_DCM_20 * m_Whl_vz;
+	m_Tir_vy = m_DCM_01 * m_Sus_vx + m_DCM_11 * m_Sus_vy + m_DCM_21 * m_Whl_vz;
+	m_Tir_vz = m_DCM_02 * m_Sus_vx + m_DCM_12 * m_Sus_vy + m_DCM_22 * m_Whl_vz;
 }
 
 void NMSPC::Tire_Fiala::update_fm (const d_vec &inputs, d_vec &outputs) {
@@ -102,14 +124,15 @@ void NMSPC::Tire_Fiala::update_fm (const d_vec &inputs, d_vec &outputs) {
 	m_Gnd_scale = inputs[1];
 	m_Tir_Prs = inputs[2];
 	m_Air_Tamb = inputs[3];
+
 	//process
 	m_sat_Fz = saturation(m_Sus_Fz, m_Fz_min, m_Fz_max);
-	m_alpha = saturation(m_alpha_prime * tanh(abs(m_Sus_vy)), m_alpha_min, \
+	m_alpha = saturation(m_alpha_prime * tanh(abs(m_Tir_vy)), m_alpha_min, \
     m_alpha_max);
     m_tan_alpha = tan(m_alpha);
     m_mu = (m_mu_max - (m_mu_max - m_mu_min) * saturation(sqrt(pow(\
     m_kappa, 2.0) + pow(m_tan_alpha, 2.0)), 0.0, 1.0)) * m_Gnd_scale;
-    m_My = -(m_aMy + abs(m_Sus_vx) * m_bMy + pow(m_Sus_vx, 2.0) * m_cMy) * \
+    m_My = -(m_aMy + abs(m_Tir_vx) * m_bMy + pow(m_Tir_vx, 2.0) * m_cMy) * \
     tanh(4.0 * m_Whl_omega) * m_Whl_Re * pow(m_sat_Fz, m_betaMy) * \
     pow(m_Tir_Prs, m_alphaMy);
     m_kappa_critical = m_mu * m_sat_Fz / 2.0 / m_cKappa;
@@ -126,26 +149,30 @@ void NMSPC::Tire_Fiala::update_fm (const d_vec &inputs, d_vec &outputs) {
     m_alpha_critical = atan(m_mu * 3.0 * m_sat_Fz / m_cAlpha);
     if (abs(m_alpha) <= m_alpha_critical) {
     	m_H = 1.0 - abs(m_tan_alpha) * m_cAlpha / (m_mu * 3.0 * m_sat_Fz);
-    	m_Mz_stick = -m_Sus_psidot * m_bMz - m_width * (1.0 - m_H) * \
+    	m_Mz_stick = -m_Tir_psidot * m_bMz - m_width * (1.0 - m_H) * \
     	pow(m_H, 3.0) * m_mu * m_sat_Fz * tanh(4.0 * m_alpha);
-    	m_Fy_stick = m_Sus_gamma * m_cGamma - (1.0 - pow(m_H, 3.0)) * m_mu *\
+    	m_Fy_stick = m_Tir_gamma * m_cGamma - (1.0 - pow(m_H, 3.0)) * m_mu *\
     	m_sat_Fz * tanh(4.0 * m_alpha);
     	m_Tir_Mz = m_Mz_stick;
     	m_Tir_Fy = m_Fy_stick;
     } else {
-    	m_Mz_slide = m_Sus_psidot * m_bMz;
+    	m_Mz_slide = m_Tir_psidot * m_bMz;
     	m_Fy_slide = -m_sat_Fz * m_mu * tanh(4.0 * m_alpha);
     	m_Tir_Mz = m_Mz_slide;
     	m_Tir_Fy = m_Fy_slide;
     }
-    m_Tir_Mx = cos(m_Sus_gamma) * m_Tir_Fy * m_Whl_Re;
+    m_Tir_Mx = cos(m_Tir_gamma) * m_Tir_Fy * m_Whl_Re;
     m_Tir_Fz = m_sat_Fz;
     m_Tir_My = m_Mroll;
 
+	//frame transfer
+	m_Sus_Fx = m_DCM_00 * m_Tir_Fx + m_DCM_01 * m_Tir_Fy + m_DCM_02 * m_Tir_Fz;
+	m_Sus_Fy = m_DCM_10 * m_Tir_Fx + m_DCM_11 * m_Tir_Fy + m_DCM_12 * m_Tir_Fz;
+	m_Sus_Fz = m_DCM_20 * m_Tir_Fx + m_DCM_21 * m_Tir_Fy + m_DCM_22 * m_Tir_Fz;
     //push outputs
-    outputs[0] = m_Tir_Fx;
-    outputs[1] = m_Tir_Fy;
-    outputs[2] = m_Tir_Fz;
+    outputs[0] = m_Sus_Fx;
+    outputs[1] = m_Sus_Fy;
+    outputs[2] = m_Sus_Fz;
     outputs[3] = m_Tir_Mx;
     outputs[4] = m_Tir_My;
     outputs[5] = m_Tir_Mz;
@@ -154,10 +181,10 @@ void NMSPC::Tire_Fiala::update_fm (const d_vec &inputs, d_vec &outputs) {
 
 void NMSPC::Tire_Fiala::update_drv (d_vec &outputs) {
 	//process
-	m_drv_kappa = (m_Whl_omega * m_Whl_Re - m_Sus_vx - abs(m_Sus_vx) * \
+	m_drv_kappa = (m_Whl_omega * m_Whl_Re - m_Tir_vx - abs(m_Tir_vx) * \
     m_kappa) / m_Lrelx;
-    m_drv_alpha_prime = (m_Sus_vy - abs(m_Sus_vx) * m_tan_alpha) / m_Lrely;
-    m_drv_Mroll = ((1.0 - abs(m_Sus_vx)) * 2 * pi + abs(m_Sus_vx - \
+    m_drv_alpha_prime = (m_Tir_vy - abs(m_Tir_vx) * m_tan_alpha) / m_Lrely;
+    m_drv_Mroll = ((1.0 - abs(m_Tir_vx)) * 2 * pi + abs(m_Tir_vx - \
     m_Whl_omega * m_Whl_Re) / saturation(m_Lrelx, 0.01, 10.0)) * \
     (m_My - m_Mroll);
 	//push outputs
