d1550b892b31fbbb3fbaff640f9ebd54d2f71e01
Caeri@hou <Caeri@hou>
2022-09-15 18:38:40 +0800

    modify gitignore.

--- src/test/simulators/simulator_tir_4fiala.cpp
+++ src/test/simulators/simulator_tir_4fiala.cpp
@@ -37,7 +37,7 @@ Simulator_Tir_4Fiala::Simulator_Tir_4Fiala(double t_start, double t_end, double 
 
 void Simulator_Tir_4Fiala::run () {
 
-	io::CSVReader<13> m_inputs("data/inputs/tir_lpf_input.csv");
+	io::CSVReader<13> m_inputs("data/inputs/tir_4fiala_inputs1.csv");
 	//io::CSVReader<13> m_inputs("data/inputs/tir_4fiala_inputs.csv");
 
 	int steps_num = static_cast<int>((m_t_end - m_t_start) / m_t_step);
